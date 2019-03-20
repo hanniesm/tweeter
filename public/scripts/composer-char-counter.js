4ab30e5e6c56399eeb2566906ec42f99d50fc16a
Hannah <hanniesutton@gmail.com>
2019-03-20 13:47:24 +0000

    Created listener that updates the character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,33 @@
-let counter = 0;
+// This is how I attempted it. It doesn't work well.
+// let counter = 0;
 
-function countDown(event) {
-  counter += 1;
-  console.log(counter)
-}
+// function countDown(event) {
+//   counter += 1;
+//   return counter
+// }
+
+// $(document).ready(function(){
+//   const frmNode = $('textarea');
+//     frmNode.on("keyup", function(event) {
+//       // event.preventDefault();
+//       // console.log(event.key);
+//     $('.counter').text(140 - countDown(event))
+//     countDown(event);
+//       // $(this).find('.counter')
+//     });
+// })
 
 $(document).ready(function(){
-  const frmNode = $('textarea');
-    frmNode.on("keyup", function(event) {
-      // event.preventDefault();
-      // console.log(event.key);
-      countDown(event);
-      // $(this).find('.counter')
-    });
+  $(".new-tweet textarea").on("input", function() {
+    const maxChar = 140
+    const currentChar = this.value.length;
+    const count = maxChar- currentChar;
+    $(".counter").text(count);
+
+    if(count < 0) {
+      $(".counter").css("color", "red");
+    } else {
+      $(".counter").css("color", "black")
+    }
+  })
 })
-
-
-
