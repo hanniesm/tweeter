c4935a8102194ff1f4a97e875a3d9fa939a18047
Hannah <hanniesutton@gmail.com>
2019-03-20 13:48:36 +0000

    changes to index file and layout to display tweets

---
+++ public/scripts/hover.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $(".tweet").hover(function(){
+    $(this).css("border-color", "black");
+    }, function(){
+    $(this).css("border-color", "darkgrey");
+  })
+});
+
