f19222607c9114c8b19dd1e24197229146527055
Hannah <hanniesutton@gmail.com>
2019-03-22 17:33:49 +0000

    Clean up single and double quote usage

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,67 +1,67 @@
 //This function takes the created tweets and adds them to the tweet section so they will appear on the page
 function renderTweets(tweets) {
-  $('#tweets').empty();
+  $("#tweets").empty();
   for (const tweetObj of tweets) {
     const $tweet = createTweetElement(tweetObj);
-    $('#tweets').prepend($tweet);
+    $("#tweets").prepend($tweet);
   }
 };
 
 //this function creates the tweet element in 3 parts, the header, body and footer
 function createTweetElement(tweet) {
-  const $tweet = $('<article>').addClass('tweet');
-  const $header = $('<header>');
-  const $leftDiv = $('<div>').addClass('leftDiv');
+  const $tweet = $("<article>").addClass("tweet");
+  const $header = $("<header>");
+  const $leftDiv = $("<div>").addClass("leftDiv");
 
-  $('<img>')
-    .attr('src', tweet.user.avatars.small)
+  $("<img>")
+    .attr("src", tweet.user.avatars.small)
     .appendTo($leftDiv);
 
-  $('<h2>')
+  $("<h2>")
     .text(tweet.user.name)
     .appendTo($leftDiv);
 
   $leftDiv.appendTo($header);
 
-  const $rightDiv = $('<div>').addClass('rightDiv');
+  const $rightDiv = $("<div>").addClass("rightDiv");
 
-  $('<p>')
+  $("<p>")
     .text(tweet.user.handle)
     .appendTo($rightDiv);
 
   $rightDiv.appendTo($header);
   $header.appendTo($tweet);
 
-  const $body = $('<div>').addClass('body');
+  const $body = $("<div>").addClass("body");
 
-  $('<p>')
+  $("<p>")
     .text(tweet.content.text)
     .appendTo($body);
 
   $body.appendTo($tweet);
 
-  const $footer = $('<footer>');
+  const $footer = $("<footer>");
 
-  $('<div>')
-    .addClass('postDate')
+  $("<div>")
+    .addClass("postDate")
     .text(tweet.created_at)
     .appendTo($footer);
 
-  const $icons = $('<div>').addClass('icons');
+  const $icons = $("<div>").addClass("icons");
 
-  $('<i>')
-    .addClass('far fa-flag')
-    .attr('id', 'flag')
+  $("<i>")
+    .addClass("far fa-flag")
+    .attr("id", "flag")
     .appendTo($icons);
 
-  $('<i>')
-    .addClass('fas fa-retweet')
-    .attr('id', 'retweet')
+  $("<i>")
+    .addClass("fas fa-retweet")
+    .attr("id", "retweet")
     .appendTo($icons);
 
-  $('<i>')
-    .addClass('far fa-heart')
-    .attr('id', 'heart')
+  $("<i>")
+    .addClass("far fa-heart")
+    .attr("id", "heart")
     .appendTo($icons);
 
   $($icons).appendTo($footer);
@@ -81,18 +81,18 @@ const request = (options, cb) => {
     })
 
     .always(() => {
-      console.log('Request completed');
+      console.log("Request completed");
     })
 };
 
 const loadTweets = () => {
   $(document).ready(function() {
-    const url = '/tweets/';
+    const url = "/tweets/";
 
     const requestOptions = {
-      method: 'GET',
+      method: "GET",
       url: url,
-      dataType: 'json',
+      dataType: "json",
     };
 
     request(requestOptions, function(response) {
@@ -105,15 +105,15 @@ loadTweets();
 
 // This posts a new tweet and loads the tweets with the new one.
 $(document).ready(function(){
-  $('#newTweet').on('submit', function() {
+  $("#newTweet").on("submit", function() {
     event.preventDefault();
-    console.log('Button clicked, performing ajax call...');
+    console.log("Button clicked, performing ajax call...");
 
     const data = $(this).serialize();
 
     const requestOptions = {
-      type: 'POST',
-      url: '/tweets/',
+      type: "POST",
+      url: "/tweets/",
       data,
     };
 
@@ -125,15 +125,15 @@ $(document).ready(function(){
 
 // This clears the form after the form is submitted
 $(document).ready(function(){
-  $( '#newTweet' ).on('submit', function() {
+  $( "#newTweet" ).on("submit", function() {
     (this).reset();
   });
 });
 
 // Also need to add to this that the textarea is enabled
 $(document).ready(function(){
-  $( '#compose' ).on('click', function() {
-    $( '.new-tweet' ).toggle('slow');
-    $(this).toggleClass('highlight');
+  $( "#compose" ).on("click", function() {
+    $( ".new-tweet" ).toggle("slow");
+    $(this).toggleClass("highlight");
   });
 });
--- public/scripts/hover.js
+++ public/scripts/hover.js
@@ -1,13 +1,13 @@
 //this is a hover function that applies to each tweet, making the opacity increase when mouse is over and reduces opacity when mouse leaves
 $(document).ready(function(){
-  $( '.tweets' ).on( 'mouseenter', '.tweet', function(event) {
-    $(this).css({'border-color': 'black', opacity: '1'});
-    $(this).find('.icons').css({opacity: '1'});
+  $( ".tweets" ).on( "mouseenter", ".tweet", function(event) {
+    $(this).css({"border-color": "black", opacity: "1"});
+    $(this).find(".icons").css({opacity: "1"});
   });
 
-  $( '.tweets' ).on( 'mouseleave', '.tweet', function(event) {
-      $(this).css({'border-color': 'darkgrey', opacity: '0.8'});
-      $(this).find('.icons').css({opacity: '0'});
+  $( ".tweets" ).on( "mouseleave", ".tweet", function(event) {
+      $(this).css({"border-color": "darkgrey", opacity: "0.8"});
+      $(this).find(".icons").css({opacity: "0"});
   });
 });
 
